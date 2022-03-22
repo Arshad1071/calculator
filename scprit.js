084e6d5124b1e1f150e688366c4b26df6225279a
Arshad1071 <Arshad1071@yahoo.in>
2022-03-22 13:44:18 +0530

    first commit

---
+++ scprit.js
@@ -0,0 +1,230 @@
+let onScr = "";
+let num1 = "";
+let num2 = "";
+var optr;
+
+document.getElementById("1").addEventListener("click", (e) => {
+    e.preventDefault();
+    if (optr == undefined) {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num1 += e.target.value;
+    } else {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num2 += e.target.value;
+    }
+});
+
+document.getElementById("2").addEventListener("click", (e) => {
+    e.preventDefault();
+    if (optr == undefined) {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num1 += e.target.value;
+    } else {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num2 += e.target.value;
+    }
+});
+
+document.getElementById("3").addEventListener("click", (e) => {
+    e.preventDefault();
+    if (optr == undefined) {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num1 += e.target.value;
+    } else {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num2 += e.target.value;
+    }
+});
+
+document.getElementById("4").addEventListener("click", (e) => {
+    e.preventDefault();
+    if (optr == undefined) {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num1 += e.target.value;
+    } else {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num2 += e.target.value;
+    }
+});
+
+document.getElementById("5").addEventListener("click", (e) => {
+    e.preventDefault();
+    if (optr == undefined) {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num1 += e.target.value;
+    } else {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num2 += e.target.value;
+    }
+});
+
+document.getElementById("6").addEventListener("click", (e) => {
+    e.preventDefault();
+    if (optr == undefined) {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num1 += e.target.value;
+
+    } else {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num2 += e.target.value;
+    }
+});
+
+document.getElementById("7").addEventListener("click", (e) => {
+    e.preventDefault();
+    if (optr == undefined) {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num1 += e.target.value;
+    } else {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num2 += e.target.value;
+    }
+});
+
+document.getElementById("8").addEventListener("click", (e) => {
+    e.preventDefault();
+    if (optr == undefined) {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num1 += e.target.value;
+    } else {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num2 += e.target.value;
+    }
+});
+
+document.getElementById("9").addEventListener("click", (e) => {
+    e.preventDefault();
+    if (optr == undefined) {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num1 += e.target.value;
+    } else {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num2 += e.target.value;
+    }
+});
+
+document.getElementById("0").addEventListener("click", (e) => {
+    e.preventDefault();
+    if (optr == undefined) {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num1 += e.target.value;
+    } else {
+        onScr += e.target.value;
+        document.getElementById('operation').value = onScr;
+        num2 += e.target.value;
+    }
+});
+
+
+
+document.getElementById("add").addEventListener("click", (e) => {
+    if (optr == undefined) {
+        e.preventDefault();
+        optr = e.target.value;
+        document.getElementById("operation").value += "+";
+        onScr += "+";
+    }
+});
+document.getElementById("sub").addEventListener("click", (e) => {
+    if (optr == undefined) {
+        e.preventDefault();
+        optr = e.target.value;
+        document.getElementById("operation").value += "-";
+        onScr += "-";
+    }
+});
+document.getElementById("mul").addEventListener("click", (e) => {
+    if (optr == undefined) {
+        e.preventDefault();
+        optr = e.target.value;
+        document.getElementById("operation").value += "x";
+        onScr += "x";
+    }
+});
+document.getElementById("div").addEventListener("click", (e) => {
+    if (optr == undefined) {
+        e.preventDefault();
+        optr = e.target.value;
+        document.getElementById("operation").value += "/";
+        onScr += "/";
+    }
+});
+
+
+//create object constructor function
+function Calculate(x, y, optr) {
+    this.x = x;
+    this.y = y;
+    this.optr = optr;
+}
+
+Calculate.prototype.result = function() {
+    let res;
+    switch (optr) {
+        case 'add':
+            res = this.x + this.y;
+            break;
+        case 'sub':
+            res = this.x - this.y;
+            break;
+        case 'mul':
+            res = this.x * this.y;
+            break;
+        case 'div':
+            res = this.x / this.y;
+            break;
+        default:
+            res = 'Error! No operation selected.';
+    }
+    return res;
+};
+
+document.getElementById("equalto").addEventListener("click", () => {
+    let cal = new Calculate(parseInt(num1), parseInt(num2), optr);
+    document.getElementById("result").value = "=" + cal.result();
+})
+
+document.getElementById("ac").addEventListener("click", () => {
+    location.reload();
+})
+
+
+document.getElementById("c").addEventListener("click", () => {
+    if (num2 != "") {
+        num2 = num2.slice(0, -1);
+        onScr = onScr.slice(0, -1);
+        document.getElementById('operation').value = onScr;
+        console.log("num1", num1, "num2", num2, onScr, "optr", optr);
+    } else {
+        if (optr != undefined) {
+            optr = undefined;
+            onScr = onScr.slice(0, -1);
+            document.getElementById('operation').value = onScr;
+            onScr = onScr.slice(0, -1);
+        } else {
+            num1 = num1.slice(0, -1);
+            onScr = onScr.slice(0, -1);
+            document.getElementById('operation').value = onScr;
+            console.log("num1", num1, "num2", num2, onScr, "optr", optr);
+        }
+    }
+})
